9e1f2a02a7b6a555620441400119d20a72d15a25
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-04-16 13:01:56 +0530

    add the explosion code when the car collide

--- EndlessDrivingGame/Assets/Scripts/Explode/ExplodeHandler.cs
+++ EndlessDrivingGame/Assets/Scripts/Explode/ExplodeHandler.cs
@@ -8,11 +8,36 @@ public class ExplodeHandler : MonoBehaviour
 
     [SerializeField]
     GameObject model;
+
+
+    Rigidbody[] rigidbodies;
+
+    private void Awake()
+    {
+        rigidbodies = model.GetComponentsInChildren<Rigidbody>(true);
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Explode(Vector3.forward);
     }
 
+    public void Explode(Vector3 externalForce)
+    {
+        originalObject.SetActive(false);  //the original car body will be set to false when the car collide with something.
+
+        foreach (Rigidbody rb in rigidbodies)
+        {
+            rb.transform.parent = null;
+
+            rb.GetComponent<MeshCollider>().enabled = true;  // here we get the mesh collider of the car pieces and set to true so it can collide.
+
+            rb.gameObject.SetActive(true);  // here the car pieces will be set to true so it will be visible when collided.
+
+            rb.isKinematic = false;
+
+            rb.AddForce(Vector3.up*200 + externalForce,ForceMode.Force);
+        }
+    }
     
 }
