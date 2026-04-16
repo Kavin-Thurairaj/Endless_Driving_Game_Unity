76ca6dfc14601aebe7fd77d0db33a051bd221d93
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-04-16 12:23:24 +0530

    create a new script to handler the explode logic when the car collide

---
+++ EndlessDrivingGame/Assets/Scripts/Explode/ExplodeHandler.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ExplodeHandler : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject originalObject;
+
+    [SerializeField]
+    GameObject model;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    
+}
