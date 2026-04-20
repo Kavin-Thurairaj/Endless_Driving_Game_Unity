cc743c7a46e5ee73d473efe8966448399f47b237
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-04-20 17:16:45 +0530

    add main me nu script

---
+++ EndlessDrivingGame/Assets/Scripts/UI/MainMenuUIHandler.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUIHandler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+   
+
+    public void OnStartGameClicked()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+}
